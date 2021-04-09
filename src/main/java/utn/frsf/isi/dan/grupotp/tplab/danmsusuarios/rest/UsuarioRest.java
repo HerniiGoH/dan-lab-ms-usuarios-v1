7885ffd1f97aaac845c2cf472f1a54726c94520c
Hernan Albornoz <calbornoz@frsf.utn.edu.ar>
2021-04-09 17:21:00 -0300

    Creado paquete repository y UsuarioRest

---
+++ src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmsusuarios/rest/UsuarioRest.java
@@ -0,0 +1,10 @@
+package utn.frsf.isi.dan.grupotp.tplab.danmsusuarios.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioRest {
+
+}
