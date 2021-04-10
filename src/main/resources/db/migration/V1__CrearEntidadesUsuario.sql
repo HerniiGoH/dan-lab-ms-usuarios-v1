399d27eac721d4ee24d26f20e322f57ced83e3a8
Hernan Albornoz <calbornoz@frsf.utn.edu.ar>
2021-04-10 12:23:00 -0300

    Errores en application.properties y en archivo de migracion corregidos.

--- src/main/resources/db/migration/V1__CrearEntidadesUsuario.sql
+++ src/main/resources/db/migration/V1__CrearEntidadesUsuario.sql
@@ -6,8 +6,8 @@ CREATE TABLE tipo_usuario
 
 CREATE TABLE usuario
 (
-    id              INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    usuario         VARCHAR(20) UNIQUE NOT NULL,
+    id              INTEGER UNSIGNED   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user            VARCHAR(20) UNIQUE NOT NULL,
     password        VARCHAR(50)        NOT NULL,
     tipo_usuario_id INTEGER UNSIGNED,
     CONSTRAINT fk_tipo_usuario_usuario FOREIGN KEY (tipo_usuario_id) REFERENCES tipo_usuario (id)
@@ -16,7 +16,8 @@ CREATE TABLE usuario
 CREATE TABLE empleado
 (
     id         INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email      VARCHAR(50)      NOT NULL,
+    mail       VARCHAR(50)      NOT NULL,
+    razon_social VARCHAR(50)    NOT NULL,
     usuario_id INTEGER UNSIGNED NOT NULL,
     CONSTRAINT fk_usuario_empleado FOREIGN KEY (usuario_id) REFERENCES usuario (id)
 );
@@ -27,7 +28,7 @@ CREATE TABLE cliente
     razon_social            VARCHAR(100)       NOT NULL,
     cuit                    VARCHAR(11) UNIQUE NOT NULL,
     mail                    VARCHAR(50) UNIQUE NOT NULL,
-    maximo_cuenta_corriente NUMERIC(19, 2),
+    max_cuenta_corriente    DOUBLE(19, 2),
     habilitado_online       BOOLEAN            NOT NULL DEFAULT FALSE,
     usuario_id              INTEGER UNSIGNED   NOT NULL,
     CONSTRAINT fk_usuario_cliente FOREIGN KEY (usuario_id) REFERENCES usuario (id)
@@ -35,7 +36,8 @@ CREATE TABLE cliente
 
 CREATE TABLE tipo_obra
 (
-    id   INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id   INTEGER UNSIGNED    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(254) NOT NULL,
     tipo VARCHAR(100) UNIQUE NOT NULL
 );
 
@@ -53,11 +55,11 @@ CREATE TABLE obra
     CONSTRAINT fk_tipo_obra_obra FOREIGN KEY (tipo_obra_id) REFERENCES tipo_obra (id)
 );
 
-INSERT INTO tipo_obra (tipo)
-VALUES ('Reforma'),
-       ('Casa'),
-       ('Edificio'),
-       ('Vial');
+INSERT INTO tipo_obra (descripcion, tipo)
+VALUES ('Obra para trabaajr en una reforma','Reforma'),
+       ('Obra para trabajar en una casa','Casa'),
+       ('Obra para trabajar en un edificio','Edificio'),
+       ('Obra para trabajar en vialidad','Vial');
 
 INSERT INTO tipo_usuario (tipo)
 VALUES ('Cliente'),
