b9b65964f488a92d247d27ad7243160cadc4c544
Hernan Albornoz <calbornoz@frsf.utn.edu.ar>
2021-05-14 19:46:45 -0300

    Cambios en ObraRepository para que la query solo revise el id de cliente

--- src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmsusuarios/service/PedidoService.java
+++
@@ -1,8 +0,0 @@
-package utn.frsf.isi.dan.grupotp.tplab.danmsusuarios.service;
-
-import utn.frsf.isi.dan.grupotp.tplab.danmsusuarios.model.Cliente;
-import utn.frsf.isi.dan.grupotp.tplab.danmsusuarios.model.DTO.PedidoDTO;
-//soy un easter egg :D
-public interface PedidoService {
-    PedidoDTO getPedidos(Cliente cliente);
-}
